0d7ff895dcbb50c4f474858c8c6f849b65d5af99
Spock7848 <spock7848@gmail.com>
2022-10-13 17:27:06 +0300

    Initial Commit

---
+++ Home Work/Lesson 1/Program.cs
@@ -0,0 +1,72 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+/*Console.WriteLine("Input two numbers: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 < num2) 
+{
+    Console.WriteLine(num2 + " is bigger then " + num1);
+}
+else if (num2 < num1)
+{
+    Console.WriteLine(num1 + " is bigger then " + num2);
+}
+else
+{
+    Console.WriteLine("Sorry, its equal ;(");
+}*/
+
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+/*Console.WriteLine("Input three numbers: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+
+if (num1 > num2 && num1 > num3)
+{
+    max = num1;
+}
+else if(num2 > num1 && num2 > num3)
+{
+    max = num2;
+}
+else
+{
+    max = num3;
+}
+Console.WriteLine("Max number is: " + max);
+*/
+
+// Долго мучался с попыткой вывода сообщения в случае повторяющихся максимальных чисел, но так ничего и не придумал.
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+/*Console.WriteLine("Input a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("Even");
+}
+else
+{
+    Console.WriteLine("Odd");
+}
+*/
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+/*Console.WriteLine("Input a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int current = 1;
+
+while(current<=num)
+{
+    if (current % 2 == 0)
+    Console.WriteLine(current);
+    current++;
+}
+*/
---
+++ Seminars/Lesson 1/Program.cs
@@ -0,0 +1,74 @@
+﻿/*
+Console.Write("Good evening everyone!");
+Console.WriteLine("Good evening everyone!");
+int n1 = 47;
+double n2 = 32.15;
+string name = "Nikita";
+bool check = true;
+Console.WriteLine(n2);
+Console.WriteLine("My number is " + n1);
+*/
+
+/*
+Console.Write("Input a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Your number is " + num);
+*/
+
+// Задача 1. Напишите программу, которая на вход принимает 2 числа и проверяет, является ли первое число квадратом второго.
+/*
+Console.Write("Input a first number: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a secind number: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+int q2 = n2 * n2;
+
+if (n1 == q2)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
+*/
+
+// Задача 2. Напишите программу, которая на выход принимает число (N), а на выходе показывает числа от -N до N.
+
+/*
+Console.Write("Input a number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int current = -n;
+
+if(n < 0)
+{
+    current = n;
+    n = -n;
+}
+
+while(current <= n)
+{
+    Console.Write(current + " ");
+    current++;
+}
+*/
+
+// Задача 3. Напишите программу, которая на вход принимает трехзначное число и на выходе показывает последнюю цифру этого числа.
+
+/*
+Console.Write("Input a three-digit number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if(n >= 100 && n <= 1000)
+{
+    int lastDigit = n % 10
+    Console.WriteLine("The last digit of " + n + " is: " + lastDigit);
+    Console.WriteLine($"The last digit of {n} is: {lastDigit}")
+}
+else
+{
+    Console.WriteLine("Uncorrect input!");
+}
+*/
