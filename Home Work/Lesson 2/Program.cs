52ef55edc91df23a33f52b7e338fac2e44fb450d
Spock7848 <spock7848@gmail.com>
2022-10-17 20:54:08 +0300

    GB Homework 2

---
+++ Home Work/Lesson 2/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+/*
+Console.Write("Input a three-digit number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if(n >= 100 && n < 1000)
+{
+    int secondDigit = n / 10 % 10;
+    Console.WriteLine($"The second digit of {n} is: {secondDigit}");
+}
+else
+{
+    Console.WriteLine("Uncorrect input!");
+}
+*/
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+/*
+Console.WriteLine("Input a number with more than 2 digits: ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+string nString = Convert.ToString(n);
+if (nString.Length > 2)
+{
+    Console.WriteLine($"The third digit of {n} is: {nString[2]}");
+}
+else {
+    Console.WriteLine("Uncorrect input!");
+}
+*/
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+/*
+Console.Write("Input a day week number: ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+void dayWeekChecker (int n)
+{
+if (n == 6 || n == 7)
+    {
+    Console.WriteLine("This day is a day off");
+    }
+else if (n < 1 || n > 7) 
+    {
+    Console.WriteLine("Uncorrect input!");
+    }
+else Console.WriteLine("This day is not a day off");
+}
+
+dayWeekChecker(n);
+*/
---
+++ Lections/Program.cs
@@ -0,0 +1,125 @@
+﻿// Функции
+
+/*
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 15;
+int b1 = 23;
+int c1 = 414;
+int a2 = 15;
+int b2 = 23;
+int c2 = 414;
+int a3 = 15;
+int b3 = 23;
+int c3 = 234234;
+
+//int max1 = Max(a1, b1, c1);   
+//int max2 = Max(a2, b2, c2); 
+//int max3 = Max(a3, b3, c3);
+
+int max = Max(
+    Max(a1, b1, c1),
+    Max(a2, b2, c2),
+    Max(a3, b3, c3));
+
+Console.WriteLine(max);
+*/
+
+// Массивы
+
+/*
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+//                0   1   2   3  4      5     6    7    8
+int[] array = { 123, 23, 32, 45, 1, 23334, 5542, 156, 973 };
+
+array[0] = 12;
+Console.WriteLine(array[0]);
+
+int max = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8]));
+
+Console.WriteLine(max);
+*/
+
+// Пример 1. Поиск позиции нужного элемента.
+
+/*
+int[] array = { 123, 23, 32, 45, 1, 23334, 5542, 45, 156, 973 };
+
+int n = array.Length;
+int find = 45;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; // Чтобы остановиться на первом попавшемся элементе
+    }
+    index++;
+}
+*/
+
+// Пример 1 но с использованием ГСЧ и методов.
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+    }   index++;
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = index;
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
