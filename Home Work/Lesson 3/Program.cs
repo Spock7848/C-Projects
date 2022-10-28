876d31aa615111657bf9c3057fb007ecdafdf953
Spock7848 <spock7848@gmail.com>
2022-10-28 19:09:45 +0300

    GB Homework 3

--- Home Work/Lesson 3/Program.cs
+++ Home Work/Lesson 3/Program.cs
@@ -16,3 +16,33 @@ Console.WriteLine($"Number {number} is a palindrom");
 else
 Console.WriteLine($"Number {number} is not a palindrom");
 */
+
+// Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+/*
+int x1 = Coordinate("X", "A");
+int y1 = Coordinate("Y", "A");
+int z1 = Coordinate("Z", "A");
+int x2 = Coordinate("X", "B");
+int y2 = Coordinate("Y", "B");
+int z2 = Coordinate("Z", "B");
+
+int Coordinate(string coordinateName, string pointName)
+{
+    Console.Write($"Enter the {coordinateName} coordinate of {pointName}: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+double Decision(double x1, double x2, 
+                double y1, double y2, 
+                double z1, double z2)
+{
+  return Math.Sqrt(Math.Pow((x2-x1), 2) + 
+                   Math.Pow((y2-y1), 2) + 
+                   Math.Pow((z2-z1), 2));
+}
+
+double segmentLength =  Math.Round (Decision(x1, x2, y1, y2, z1, z2), 2 );
+
+Console.WriteLine($"Segment length is {segmentLength}");
+*/
