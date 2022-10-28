e94e7aac5501e0c76954b5a04efe5b3b205dc660
Spock7848 <spock7848@gmail.com>
2022-10-28 18:42:29 +0300

    GB Homework 3

---
+++ Seminars/Lesson 2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 1. Напишите программу, которая на вход принимает двузначное число и показывает наибольшую цифру этого числа.
+
+int FindBiggerDigit(int num)
+
+{
+    int result;
+
+    if(num < 10 || num >= 100)
+        return 0;
+    else
+    {
+        int ed = num % 10;
+        int dec = num / 10;
+
+        if(ed > dec)
+            result = ed;
+        else
+            result = dec;
+    }
+    
+    return result;
+}
+
+Console.Write("Input a two-digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int biggerDigit = FindBiggerDigit(number);
+Console.WriteLine($"Bigger digit on {number} is {biggerDigit}");
