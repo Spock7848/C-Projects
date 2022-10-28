8e31f5069633d9bf72b4d7c95a3d54b725987be9
Spock7848 <spock7848@gmail.com>
2022-10-28 18:46:21 +0300

    GB Homework 3

--- Home Work/Lesson 3/Program.cs
+++ Home Work/Lesson 3/Program.cs
@@ -1,5 +1,6 @@
 ﻿// Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 
+/*
 Console.Write("Input a five-digit number: ");
 int number = Convert.ToInt32(Console.ReadLine()); 
 int num1 = number / 10000 % 10;
@@ -8,9 +9,10 @@ int rev1 = number / 10 % 10;
 int rev2 = number % 10;
 int nul = number / 10000;
         
-    if (nul < 1 || nul > 9)
-    Console.WriteLine($"Number {number} is not a five-digit number");
-    else if  (num1 == rev2 && num2 == rev1)
-    Console.WriteLine($"Number {number} is a palindrom");
-    else
-    Console.WriteLine($"Number {number} is not a palindrom");
+if (nul < 1 || nul > 9)
+Console.WriteLine($"Number {number} is not a five-digit number");
+else if  (num1 == rev2 && num2 == rev1)
+Console.WriteLine($"Number {number} is a palindrom");
+else
+Console.WriteLine($"Number {number} is not a palindrom");
+*/
