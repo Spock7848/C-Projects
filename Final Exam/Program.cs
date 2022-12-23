28833d699249b17347adcf4a511e26ff0981b1ae
Spock7848 <spock7848@gmail.com>
2022-12-24 01:06:58 +0300

    Void Array created

--- Final Exam/Program.cs
+++ Final Exam/Program.cs
@@ -1,2 +1,14 @@
-﻿string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
-string[] array2 = new string[array1.Length];
+﻿string[] firstArray = new string[5] {"123", "23", "hello", "world", "res"};
+string[] secondAarray = new string[firstArray.Length];
+void Array(string[] firstArray, string[] secondArray)
+{
+    int count = 0;
+    for (int i = 0; i < firstArray.Length; i++)
+    {
+    if(firstArray[i].Length <= 3)
+        {
+        secondArray[count] = firstArray[i];
+        count++;
+        }
+    }
+}
