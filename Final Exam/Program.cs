1c456dc5354f1a0ae7e0acf26604c57365f53501
Spock7848 <spock7848@gmail.com>
2022-12-24 01:18:52 +0300

    Initial Commit

--- Final Exam/Program.cs
+++ Final Exam/Program.cs
@@ -1,25 +1,3 @@
-﻿string[] firstArray = new string[5] {"123", "23", "hello", "world", "res"};
-string[] secondAarray = new string[firstArray.Length];
-void Array(string[] firstArray, string[] secondArray)
-{
-    int count = 0;
-    for (int i = 0; i < firstArray.Length; i++)
-    {
-    if(firstArray[i].Length <= 3)
-        {
-        secondArray[count] = firstArray[i];
-        count++;
-        }
-    }
-}
-
-void PrintArray(string[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write($"{array[i]} ");
-    }
-    Console.WriteLine();
-}
-SecondArrayWithIF(firstArray, secondArray);
-PrintArray(secondArray);
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string[] firstArray = new string[5] {"123", "23", "hello", "world", "res"};
