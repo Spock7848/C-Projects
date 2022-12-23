0ab96f1616346e628336ac9d0c7bc7c56c25a7c9
Spock7848 <spock7848@gmail.com>
2022-12-24 02:19:25 +0300

    Void Top3PL2023 created

--- Final Exam/Program.cs
+++ Final Exam/Program.cs
@@ -1,2 +1,15 @@
 ﻿string[] firstArray = new string[5] {"Arsenal", "Man City", "Liverpool", "Newcastle United", "Leicester City"};
 string[] secondArray = new string[firstArray.Length];
+
+void Top3PL2023(string[] firstArray, string[] secondArray)
+{
+    int count = 0;
+    for (int i = 0; i < firstArray.Length; i++)
+    {
+    if(firstArray[i].Length <= 3)
+        {
+        secondArray[count] = firstArray[i];
+        count++;
+        }
+    }
+}
